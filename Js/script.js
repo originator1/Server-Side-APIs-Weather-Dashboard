e874ca5fd2b1dad7fa11281126f9a51a843380f6
James Garinger <jkggaringer@gmail.com>
2022-02-02 12:09:35 -0600

    refactoring repetative destructuring, cleaning up responsive design and ui appeal

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+var hiddenEls = document.querySelectorAll(".hide");
 //grab search inputText box
 var inputText = document.querySelector("#inputText");
 //grab search button
@@ -7,15 +8,18 @@ var historyBox = document.querySelector(".historyBox");
 
 var searchNameEl = document.querySelector('#searchName');
 
+//ONE DAY FORECAST ------------------------------------------------
 var oneDayTempEl = document.querySelector('#oneDayTemp');
 var oneDayWindEl = document.querySelector('#oneDayWind');
 var oneDayHumEl = document.querySelector('#oneDayHum');
 var oneDayUvEl = document.querySelector('#oneDayUV');
-
+//FIVE DAY FORECAST------------------------------------------------
 var fiveDayDiv = document.querySelector('#fiveDay');
 
+//CREATE EMPTY ARRAY FOR CITIES SEARCHED(BUTTONS); IF CITY NOT ALREADY IN ARRAY, NEW SEARCH BUTTON PUSHED TO LIST--
 var cityArray = [];
 console.log(cityArray);
+
 //one call api current uv index: current.uvi
 //https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
 
@@ -24,6 +28,9 @@ console.log(cityArray);
 function searchCity(valueText) {
   //event.preventDefault();
   //var valueText = inputText.value;
+  hiddenEls.forEach((el)=> {
+    el.classList.remove('hide');
+  })
 
   var oneDayQueryUrl =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -36,7 +43,7 @@ function searchCity(valueText) {
     })
     .then(function (data) {
       savingCity(valueText);
-      console.log(data);
+      console.log("my data",data);
       let searchName = data.name;
       console.log("checking searchname: " + searchName);
       let temp = data.main.temp;
@@ -56,36 +63,37 @@ function searchCity(valueText) {
       })
       .then (function(fiveData) {
         console.log("This is my 5 day object");
-        console.log(fiveData);
+        console.log("my fiveDay data obj from api", fiveData);
         
-        
-        let dayOne = fiveData.list[0];
-        let dayTwo = fiveData.list[1];
-        let dayThree = fiveData.list[2];
-        let dayFour = fiveData.list[3];
-        let dayFive = fiveData.list[4];
+        dayObjects = [];
+        //can adjust amount of days by changing incrementor(i)
+        for (i = 0; i < 5; i++) {
+           dayObjects.push(fiveData.list[i]);
+        }
+
         
         const newFiveDiv = document.querySelector('.fiveDay');
-        
+       
+
         if (newFiveDiv.innerHTML !== null) {
           newFiveDiv.innerHTML = "";
         };
 
-        dayObjects = [dayOne, dayTwo, dayThree, dayFour, dayFive];
+        // dayObjects = [dayOne, dayTwo, dayThree, dayFour, dayFive];
         dayObjects.map((day) => {
           let temp = day.main.temp; 
           let hum = day.main.humidity;
           let wind = day.wind.speed;
-          
+          console.log(day);
           const newFiveDiv = document.querySelector('.fiveDay');
-
-         
+          let count = 1;
+          let fTemp = parseInt(((temp - 273.15) * 9/5) + 32);
 
           newFiveDiv.insertAdjacentHTML('afterbegin', 
-          `<div class="genDiv">
-              <p>temp: <span id='twoTemp'>${temp}</span></p>
-              <p>wind: <span id='twoWind'>${hum}</span></p>
-              <p>Humidity: <span id='twoHum'>${wind}</span></p>
+          `<div class="genDiv" style="font-size: 1rem">
+              <p<span id='twoTemp'>${fTemp}${String.fromCharCode(176) + "F"}</span></p>
+              <p><span id='twoWind'>Hum: ${hum}</span></p>
+              <p><span id='twoHum'>${wind}MPH Winds</span></p>
           </div>`); 
         });
 
@@ -117,9 +125,7 @@ function appendOneDay(temp, searchName, wind, humidity) {
   // oneDayUvEl.append()
 }; 
 
-function appendDayOne() {
 
-}
 
 
 //push that city into array
@@ -147,12 +153,12 @@ function displayCities() {
   var searchedCities = JSON.parse(localStorage.getItem("savedSearches")) || [];
   //clear the area out before adding new buttons to avoid duplications;
   historyBox.innerHTML="";
-  console.log(searchedCities);
+  console.log("results from localstorage get",searchedCities);
 
   searchedCities.forEach((cityEl) => {
-    console.log(cityEl);
+    console.log("each element in searched cities(localstorage)", cityEl);
     var cityBtn = document.createElement("button");
-    cityBtn.setAttribute('class', 'generatedBtn')
+    cityBtn.setAttribute('class', 'generatedBtn btn-primary')
     cityBtn.textContent = cityEl;
     //add class to the buttons
     cityBtn.classList.add("pastCities");
@@ -161,8 +167,9 @@ function displayCities() {
     historyBox.appendChild(cityBtn);
   });
 }
-
+//FETCHES API DATA WHEN ONE OF PREVIOUS SEARCHED CITY BUTTONS CLICKED FROM cityArray
 document.querySelector(".historyBox").addEventListener("click", function(event){
+  event.preventDefault();
     //event is the click event
     //event.target is the item we clicked on
     //ever.target.value will return the value of the value attribute of that item
@@ -173,10 +180,10 @@ document.querySelector(".historyBox").addEventListener("click", function(event){
 })
 
 displayCities()
-
+//FETCHES API DATA WHEN SEARCH BUTTON CLICKED
 searchBtn.addEventListener("click", function(){
     var searchedCityValue = inputText.value;
-    //pass the value of searchedCityValue to searchCity function as an argument
+    //runs search city api function on inputText value of past city clicked
     searchCity(searchedCityValue);
 });
 
